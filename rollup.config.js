704b418c128628ebe9f0408bf51922355bebda93
avi12 <avi6106@gmail.com>
2021-01-02 12:11:21 +0200

    Now processing files individually instead of relying on "rollup-plugin-chrome-extension"

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,84 @@
-import {
-  chromeExtension,
-  simpleReloader,
-} from "rollup-plugin-chrome-extension";
-
-import preprocess from "svelte-preprocess";
-import svelte from "rollup-plugin-svelte";
-import postcss from "rollup-plugin-postcss";
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import zip from "rollup-plugin-zip";
-import { emptyDir } from "rollup-plugin-empty-dir";
+import svelte from "rollup-plugin-svelte";
 import { terser } from "rollup-plugin-terser";
+import css from "rollup-plugin-css-only";
+import postcss from "rollup-plugin-postcss";
 import json from "@rollup/plugin-json";
+import commonjs from "@rollup/plugin-commonjs";
+import preprocess from "svelte-preprocess";
 
-const production = !process.env.ROLLUP_WATCH;
-export default {
-  input: "src/manifest.json",
-  output: {
-    dir: "dist",
-    format: "esm",
-  },
-  plugins: [
-    json(),
-    chromeExtension(),
-    !production && simpleReloader(),
-    resolve({
-      browser: true,
-      dedupe: ["svelte"],
-    }),
-    commonjs(),
-    resolve(),
-    svelte({
-      compilerOptions: {
-        dev: !production,
-      },
-      preprocess: preprocess({
-        scss: {
-          includePaths: ["theme"],
-        },
+const isProduction = !process.env.ROLLUP_WATCH;
+
+function createConfig(filename, useSvelte = false) {
+  return {
+    input: `src/${filename}.js`,
+    output: {
+      format: "esm",
+      file: `dist/build/${filename}.js`
+    },
+    plugins: [
+      useSvelte && css({ output: "bundle.css" }),
+      useSvelte &&
+        svelte({
+          // enable run-time checks when not in production
+          compilerOptions: {
+            dev: !isProduction
+          },
+          preprocess: preprocess({
+            scss: {
+              includePaths: ["theme"]
+            }
+          })
+          // we'll extract any component CSS out into
+          // a separate file - better for performance
+        }),
+
+      json(),
+
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration -
+      // consult the documentation for details:
+      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+      commonjs(),
+      resolve({
+        browser: true,
+        dedupe: ["svelte"]
       }),
-    }),
-    postcss(),
-    emptyDir(),
-    production && terser(),
-    production && zip({ dir: "dist_packed" }),
-  ],
-  watch: {
-    clearScreen: false,
-  },
-};
+
+      // If we're building for production (npm run build
+      // instead of npm run dev), minify
+      isProduction && terser()
+    ],
+    watch: {
+      clearScreen: false
+    }
+  };
+}
+
+function createConfigCss(filename) {
+  return {
+    input: `src/styles-injected/${filename}.css`,
+    output: {
+      file: `dist/build/styles-injected/${filename}.css`
+    },
+    plugins: [
+      postcss({
+        extract: true
+      })
+    ],
+    watch: {
+      clearScreen: false
+    }
+  };
+}
+
+export default [
+  createConfig("popup/popup", true),
+  createConfig("scripts/background"),
+  createConfig("scripts/content-script-steam-friends-manager"),
+  createConfig("scripts/content-script-steam-initialize"),
+  createConfig("scripts/content-script-steam-user"),
+  createConfig("scripts/utilities"),
+  createConfigCss("main")
+];
